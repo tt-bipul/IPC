27cf81a4d41a545364abae1bdf04114c5d3e5bfa
bipulsardar421 <bipulsardar421@gmail.com>
2026-01-08 12:15:24 +0530

    Tractor

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -10,7 +10,7 @@ const options: swaggerJsdoc.Options = {
         },
         servers: [
             {
-                url: 'http://localhost:3000',
+                url: 'https://ipc-black.vercel.app/',
                 description: 'Local server',
             },
         ],
